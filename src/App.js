0a66cc85595cbae63b54c0fb85335f01f449d64b
shuhsuan <shuhsuanchang.9@gmail.com>
2023-02-19 00:04:21 +0800

    still a bit confused

--- src/App.js
+++ src/App.js
@@ -1,6 +1,60 @@
-import React from "react";
+import React from 'react';
 //This js contains all the stuff to render
 
+class MyApp extends React.Component{
+  constructor(props){
+    super(props)
 
+    this.state={
+      input: ""
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+handleChange(event){
+  this.setState({
+    input: event.target.value
+  });
+}
+  render(){
+    return(
+      <div>
+        <Editor input={this.state.input} handleChange={this.handleChange}/>
+        <Preview input={this.state.input}/>
+      </div>
+    )
+  }
+}
 
+class Editor extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div>
+      <h1>Editor</h1>
+      
+      <textarea id="editor" value={this.props.input} onChange={this.props.handleChange}></textarea>
+     
+      </div>
+  )
+  
+  };
+}
+
+class Preview extends React.Component {
+  constructor(props){
+    super(props)
+  }
+render(){
+  return(
+    <div>
+    <h1>Previewer</h1>
+    <p id="preview">{this.props.input}</p>
+    </div>
+  )
+}
+}
+
+export default MyApp;
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //there's a div in html called root which this renders to
 root.render(
