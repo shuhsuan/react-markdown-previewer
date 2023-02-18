88d1a5fe0cc6d3150ad2210f3d23f213bdc1e5a0
shuhsuan <shuhsuanchang.9@gmail.com>
2023-02-18 15:45:31 +0800

    redo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,4 @@
 import React from "react";
-import Badge from "react-bootstrap/Badge";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <h1>
-            <Badge className="text-align-center" variant="light">
-              Markdown Previewer
-            </Badge>
-          </h1>
-
-        </div>
-      </div>
-    )
-  }
-}
 
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); //there's a div in html called root which this renders to
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//This js takes what to render and where to render
